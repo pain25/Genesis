f7a3a1a59b65e9f3294c925ff250b444ad6959b9
Nilesh Gautam <gautamnilesh084@gmail.com>
2022-12-20 15:03:57 +0530

    updated the app

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,20 +1,28 @@
-
-
-const observer = new IntersectionObserver((enteries)=> {
-  enteries.forEach((entry)=>{
-if (entry.isIntersecting) {
-entry.target.classList.add('show');
-
-}else {
-entry.target.classList.remove('show')
-
-}
+const observer = new IntersectionObserver((enteries) => {
+  enteries.forEach((entry) => {
+    if (
+      entry.isIntersecting && entry.target.classList.contains("notvissible")
+    ) {
+      entry.target.classList.add("show");
+    } else {
+      if (entry.isIntersecting && entry.target.classList.contains("cardGlow")) {
+        setTimeout(() => {
+          entry.target.classList.add("scroll");
+        }, 600);
+      } else {
+        entry.target.classList.remove("scroll");
+      }
+    }
   });
-}
-);
+});
 
-const hiddenElements = document.querySelectorAll('.notvissible');
-hiddenElements.forEach((el)=> observer.observe(el));
+const hiddenElements = document.querySelectorAll(".notvissible");
+hiddenElements.forEach((el) => observer.observe(el));
+
+const cardGlow = document.querySelectorAll(".cardGlow");
+cardGlow.forEach((cd) => {
+  observer.observe(cd);
+});
 
 let current = 0;
 
@@ -23,58 +31,52 @@ const tabs = document.querySelectorAll(".btn li");
 const prevEl = document.querySelector("#prev");
 const nextEl = document.querySelector("#next");
 
+const toggletabs = () => {
+  tabs.forEach((tab) => {
+    tab.classList.remove("tactive");
+  });
+  tabs[current].classList.add("tactive");
+};
 
-const toggletabs=()=>{
-tabs.forEach((tab)=>{
-tab.classList.remove("tactive");
-})
-tabs[current].classList.add("tactive");
-}
+const togglecontainer = () => {
+  containerEl.forEach((container) => {
+    container.classList.remove("active");
+  });
+  containerEl[current].classList.add("active");
+};
 
-const togglecontainer=()=>{
-containerEl.forEach((container)=>{
-container.classList.remove("active");
-})
-containerEl[current].classList.add("active");
-}
+const toggleprev = () => {
+  if (current == 0) {
+    prevEl.disabled = true;
+  } else {
+    prevEl.disabled = false;
+  }
+};
 
-const toggleprev=()=>{
-if(current == 0){
-prevEl.disabled = true;
-}else{
-prevEl.disabled = false;
-}
-}
+const togglenext = () => {
+  if (current == tabs.length - 1) {
+    nextEl.disabled = true;
+  } else {
+    nextEl.disabled = false;
+  }
+};
 
-const togglenext=()=>{
-if(current == tabs.length -1){
-nextEl.disabled = true;
-}else{
-nextEl.disabled = false;
-}
-}
+const next = () => {
+  if (current < tabs.length - 1) {
+    current++;
 
-const next=()=>{
-if(current < tabs.length-1){
-current++
-
-
-togglecontainer();
-toggletabs();
-toggleprev();
-togglenext();
-}
-}
-const prev=()=>{
-if(current > 0){
-current--
-}
-togglecontainer();
-toggletabs();
-toggleprev();
-togglenext();
-
-}
-
-
-
+    togglecontainer();
+    toggletabs();
+    toggleprev();
+    togglenext();
+  }
+};
+const prev = () => {
+  if (current > 0) {
+    current--;
+  }
+  togglecontainer();
+  toggletabs();
+  toggleprev();
+  togglenext();
+};
--- JavaScript/navfunc.js
+++ JavaScript/navfunc.js
@@ -1,7 +1,7 @@
 const hamburgerEl = document.querySelector("#hamburger");
 const sidebarEl = document.querySelector("#sidebar");
 
-document.onclick = function(e){
+window.onclick = function(e){
     console.log(e.target.id)
     if(e.target.id !== 'sidebar' && e.target.id !== 'hamburger'  ){
             sidebarEl.classList.remove("open")
@@ -19,11 +19,11 @@ hamburgerEl.onclick =function(){
 
 const eventTableEl = document.querySelector("#eventTable");
 
-window.addEventListener("resize", ()=>{
-  console.log(screen.width)
-  if(screen.width <= 480){
-    eventTableEl.classList.remove("notvissible")
-  }else{
-    eventTableEl.classList.add("notvissible")
-  }
-})
+// window.addEventListener("resize", ()=>{
+//   console.log(screen.width)
+//   if(screen.width <= 480){
+//     eventTableEl.classList.remove("notvissible")
+//   }else{
+//     eventTableEl.classList.add("notvissible")
+//   }
+// })
